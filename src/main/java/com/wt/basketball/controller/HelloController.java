7c01cd71754b8dd7b67ca78a9f13a55020dc6f37
wut <806947301@qq.com>
2021-03-17 23:47:15 +0800

    feat(user): 增加用户表以及mapper

--- src/main/java/com/wt/basketball/controller/HelloController.java
+++ src/main/java/com/wt/basketball/controller/HelloController.java
@@ -1,7 +1,12 @@
 package com.wt.basketball.controller;
 
+import com.wt.basketball.dao.UserMapper;
+import com.wt.basketball.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author wut
@@ -9,8 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
+    @Autowired
+    UserMapper userMapper;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello world";
+    }
+
+    @GetMapping("/test")
+    public List test() {
+        List<User> users = userMapper.selectAll();
+
+        return users;
     }
 }
